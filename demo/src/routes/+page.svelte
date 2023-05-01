b1708f1a91b1e64be4bad9eb3e1e07d27539d123
BearToCode <davidebasso03@gmail.com>
2023-05-01 15:33:25 +0200

    fix block katex

--- demo/src/routes/+page.svelte
+++ demo/src/routes/+page.svelte
@@ -10,4 +10,63 @@
 	});
 </script>
 
-<CartaEditor {carta} />
+<svelte:head>
+	<!-- Custom font -->
+	<link rel="preconnect" href="https://fonts.googleapis.com" />
+	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+	<link
+		href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap"
+		rel="stylesheet"
+	/>
+</svelte:head>
+
+<main>
+	<CartaEditor {carta} />
+</main>
+
+<style>
+	:global(body) {
+		margin: 0;
+		font-family: 'Fira Code', monospace;
+		font-variant-ligatures: normal;
+		min-height: 100vh;
+	}
+
+	main {
+		max-width: 1536px;
+		margin: 0 auto 0 auto;
+		padding: 2rem 0 2rem 0;
+	}
+
+	/* Responsive main */
+
+	@media screen and (max-width: 640px) {
+		main {
+			width: 95%;
+		}
+	}
+
+	@media screen and (min-width: 640px) and (max-width: 767px) {
+		main {
+			width: 640px;
+		}
+	}
+
+	@media screen and (min-width: 767px) and (max-width: 1023px) {
+		main {
+			width: 768px;
+		}
+	}
+
+	@media screen and (min-width: 1023px) and (max-width: 1279px) {
+		main {
+			width: 1024px;
+		}
+	}
+
+	@media screen and (min-width: 1279px) and (max-width: 1535px) {
+		main {
+			width: 1280px;
+		}
+	}
+</style>
