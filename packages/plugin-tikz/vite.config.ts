b2fb616883caeb3a8bd883eb2999e0821625de95
BearToCode <davidebasso03@gmail.com>
2023-07-23 19:03:09 +0200

    don't externalize md5 but move it to dev deps

--- packages/plugin-tikz/vite.config.ts
+++ packages/plugin-tikz/vite.config.ts
@@ -6,14 +6,6 @@ import rawLoader from 'vite-raw-plugin';
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
-		rollupOptions: {
-			external: ['md5'],
-			output: {
-				globals: {
-					md5: 'md5'
-				}
-			}
-		},
 		lib: {
 			entry: resolve(__dirname, 'src/index.ts'),
 			name: 'plugin-tikz',
