5036ad37d1921a2b3b478ad9f1342f9918286891
BearToCode <davidebasso03@gmail.com>
2023-08-05 16:14:22 +0200

    log stdout, stderr when package build fails

--- scripts/build.js
+++ scripts/build.js
@@ -8,10 +8,13 @@ spinner.color = 'red';
 
 for (const [index, pkg] of packages.entries()) {
 	spinner.text = `Building ${pkg} [${index + 1}/${packages.length}]`;
+	let stdout, stderr;
 	try {
-		await execAsync(`cd packages/${pkg} && npm run build`);
+		({ stdout, stderr } = await execAsync(`cd packages/${pkg} && npm run build`));
 	} catch (e) {
 		spinner.fail(`Failed to build ${pkg}: \n ${e}`);
+		console.log(`\nstdout:\n\n${stdout}`);
+		console.error(`\nstderr:\n\n${stderr}`);
 		process.exit(1);
 	}
 }
