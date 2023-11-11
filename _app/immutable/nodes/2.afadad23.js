8dff36cf38e857de9180484edc016486651423d8
BearToCode <BearToCode@users.noreply.github.com>
2023-11-11 10:01:36 +0000

    deploy: 06eebf8c93524fbc525930e8953035c4ebfe2cc9

---
+++ _app/immutable/nodes/2.afadad23.js
@@ -0,0 +1,1 @@
+import{P as e,_ as n}from"../chunks/2.fb4d42a4.js";export{e as component,n as universal};
--- _app/immutable/nodes/2.f9b71722.js
+++
@@ -1,1 +0,0 @@
-import{P as e,_ as n}from"../chunks/2.15b2482a.js";export{e as component,n as universal};
