345c98727334fe8b939752940e46b86a09892f15
BearToCode <davidebasso03@gmail.com>
2023-11-19 23:54:39 +0100

    docs: fix href

--- docs/src/routes/+page.svelte
+++ docs/src/routes/+page.svelte
@@ -1,8 +1,9 @@
 <script lang="ts">
 	import { goto } from '$app/navigation';
+	import { base } from '$app/paths';
 	import { onMount } from 'svelte';
 
 	onMount(() => {
-		goto('./introduction');
+		goto(`${base}/introduction`);
 	});
 </script>
