1b75fe794e184818f8b1bca390dc073979de3aa1
Davide <47280851+BearToCode@users.noreply.github.com>
2023-12-16 23:23:38 +0100

    feat: add custom labels support (#23)
    * feat: add custom labels
    * build: pin prettier to v3.1.0

---
+++ packages/carta-md/src/lib/internal/labels.ts
@@ -0,0 +1,12 @@
+/**
+ * Labels that may appear in the editor.
+ */
+export interface CartaLabels {
+	writeTab: string;
+	previewTab: string;
+}
+
+export const DefaultCartaLabels: CartaLabels = {
+	writeTab: 'Write',
+	previewTab: 'Preview'
+};
