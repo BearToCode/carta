89e101c27824846f10944a064ae7317ba237c86a
BearToCode <BearToCode@users.noreply.github.com>
2024-01-06 11:32:51 +0000

    deploy: 5ddddbdc2e6a9864b471fcbbdfa79cc64a2af041

---
+++ _app/immutable/chunks/navigation.1c5e2ef4.js
@@ -0,0 +1,1 @@
+import{l as o}from"./singletons.ba8e1b3a.js";const e=o("goto");export{e as g};
--- _app/immutable/nodes/2.bc654a66.js
+++
@@ -1,1 +0,0 @@
-import{s as n,o as s}from"../chunks/scheduler.a9223a08.js";import{S as e,i as r}from"../chunks/index.ee2268cf.js";import{g as a}from"../chunks/navigation.acaae790.js";import{e as i}from"../chunks/singletons.4b45b5f6.js";function m(o){return s(()=>{a(`${i}/introduction`)}),[]}class l extends e{constructor(t){super(),r(this,t,m,null,n,{})}}export{l as component};
