fd83e003c560b9d8661a04b28cd32c23e606ff7a
BearToCode <davidebasso03@gmail.com>
2024-06-05 21:56:14 +0200

    feat: add PreRendered component

---
+++ packages/carta-md/src/lib/PreRendered.svelte
@@ -0,0 +1,14 @@
+<!--
+  @component
+  A basic component to render pre-rendered HTML content with a specific theme.
+-->
+
+<script lang="ts">
+	export let html: string;
+	export let theme: string;
+</script>
+
+<div class="carta-viewer carta-theme__{theme} markdown-body">
+	<!-- eslint-disable-next-line svelte/no-at-html-tags -->
+	{@html html}
+</div>
