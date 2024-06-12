2e572f7d0f3b72efdcf3372e9b6bb4dbc4990bc1
Warren R. Bank <warren.r.bank@gmail.com>
2024-06-12 02:35:24 -0700

    update prefixes: both method signature and default implementations
    method signature:
    =================
    * update "prefix.match(line)" return type
      - to: string | undefined | RegExpExecArray | null
      - benefit:
        * to support passing a regex match with capture groups
          - from: "prefix.match(line)"
          - to:   "prefix.maker(match, line)"
    
    default implementations:
    ========================
    * allow leading whitespace to support nesting of elements
    * better support for detecting valid markdown syntax

--- packages/carta-md/src/lib/internal/prefixes.ts
+++ packages/carta-md/src/lib/internal/prefixes.ts
@@ -12,44 +12,43 @@ export interface Prefix {
 	 * @param line The current line.
 	 * @returns The prefix, or nothing if it was not.
 	 */
-	match: (line: string) => string | undefined;
+	match: (line: string) => string | undefined | RegExpExecArray | null;
 	/**
 	 * Function that returns the prefix for the new line.
 	 * @param previousMatch The prefix matched in the `match` function.
 	 * @param previousLine The previous line.
 	 * @returns The new prefix.
 	 */
-	maker: (previousMatch: string, previousLine: string) => string;
+	maker: (previousMatch: string | RegExpExecArray, previousLine: string) => string;
+}
+
+const matchRegexs = {
+	taskList:     /^(\s*)(\-\s+\[)[ xX]?(\])/,
+	bulletedList: /^(\s*)([\-\*]\s+)/,
+	numberedList: /^(\s*)(\d+)(\.\s+)/,
+	blockquote:   /^(\s*)([\> ]*[\>]\s+)/
 }
 
 export const defaultPrefixes = [
 	{
 		id: 'taskList',
-		match: (line) => {
-			const prefix = line.slice(0, 5);
-			if (prefix.includes('- []')) return prefix;
-			if (prefix.includes('- [ ]')) return prefix;
-			if (prefix.includes('- [x]')) return prefix;
-		},
-		maker: () => '- [ ] '
+		match: (line) => matchRegexs.taskList.exec(line),
+		maker: (prev) => `${prev[1]}${prev[2]} ${prev[3]}`
 	},
 	{
 		id: 'bulletedList',
-		match: (line) => {
-			const prefix = line.slice(0, 2);
-			if (prefix === '- ') return prefix;
-		},
-		maker: () => '- '
+		match: (line) => matchRegexs.bulletedList.exec(line),
+		maker: (prev) => `${prev[1]}${prev[2]}`
 	},
 	{
 		id: 'numberedList',
-		match: (line) => line.match(/^\d+\./)?.at(0),
-		maker: (prev) => `${Number(prev.slice(0, -1)) + 1}. `
+		match: (line) => matchRegexs.numberedList.exec(line),
+		maker: (prev) => `${prev[1]}${Number(prev[2]) + 1}${prev[3]}`
 	},
 	{
 		id: 'blockquote',
-		match: (line) => line.match(/^(> *)+/gm)?.at(0),
-		maker: (prev) => prev
+		match: (line) => matchRegexs.blockquote.exec(line),
+		maker: (prev) => `${prev[1]}${prev[2]}`
 	}
 ] as const satisfies readonly Prefix[];
 
